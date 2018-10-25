350c2c7af9f0120cbbd9ee4441b38d15e022db27
Ben Buttigieg <benbtg@gmail.com>
2018-10-25 19:25:09 +0100

    Initial commit

---
+++ NicWrites/Views/ScreenplaysPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace NicWrites.Views
+{
+    public class ScreenplaysPage : ContentPage
+    {
+        public ScreenplaysPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
