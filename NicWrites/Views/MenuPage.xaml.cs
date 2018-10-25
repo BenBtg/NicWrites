eceae9440856b433d2b8330525e56c874fdc4b84
Ben Buttigieg <benbtg@gmail.com>
2018-10-25 19:25:47 +0100

    Added Screenplay

--- NicWrites/Views/MenuPage.xaml.cs
+++ NicWrites/Views/MenuPage.xaml.cs
@@ -18,7 +18,11 @@ namespace NicWrites.Views
 
             menuItems = new List<HomeMenuItem>
             {
-                new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
+                new HomeMenuItem {Id = MenuItemType.About, Title="About Nic" },
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Short Stories" },
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Screenplays" },
+                new HomeMenuItem {Id = MenuItemType.Browse, Title="Social Media" },
+                new HomeMenuItem {Id = MenuItemType.About, Title="Copy" },
                 new HomeMenuItem {Id = MenuItemType.About, Title="About" }
             };
 
--- NicWrites/Views/ScreenplaysPage.cs
+++ NicWrites/Views/ScreenplaysPage.cs
@@ -11,7 +11,7 @@ namespace NicWrites.Views
             Content = new StackLayout
             {
                 Children = {
-                    new Label { Text = "Hello ContentPage" }
+                    new Label { Text = "Screenplays" }
                 }
             };
         }
