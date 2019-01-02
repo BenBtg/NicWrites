5e56090139443fd8d11a196f36fc75654eb56ec9
Ben Buttigieg <benbtg@gmail.com>
2019-01-02 15:50:41 +0000

    Added generic document content page

---
+++ Xamarin/NicWrites/ViewModels/DocumentContentViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.MobCAT;
+using NicWrites.Models;
+using NicWrites.Services;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Microsoft.AppCenter.Analytics;
+
+namespace NicWrites.ViewModels
+{
+    public class DocumentContentViewModel: BaseNicWritesViewModel
+    { 
+        public object ViewModel
+        {
+            get;
+            set;
+        }
+
+        public string WebContent
+        {
+            get => _webContent;
+            set { RaiseAndUpdate(ref _webContent, value); }
+        }
+
+        private INicWritesService _nicWritesService;
+
+        string _webContent;
+
+        public DocumentContentViewModel()
+        {
+            _nicWritesService = ServiceContainer.Resolve<INicWritesService>();
+        }
+
+        public async override Task InitAsync()
+        {
+
+            try
+            {
+                IsBusy = true;
+                var result = await _nicWritesService.GetScreenplaysAsync();
+                var sourceText = result[1].content;
+
+                Analytics.TrackEvent("ViewScript " + result[1].title);
+
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    // Code to run on the main thread
+                    WebContent = sourceText;
+                    ;
+                });
+            
+            }
+            catch (Exception ex)
+            {
+                Logger.Debug(ex.Message, ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ Xamarin/NicWrites/Views/DocumentContentPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.MobCAT.Forms.Pages;
+using Xamarin.Forms;
+
+namespace NicWrites.Views
+{
+    public partial class DocumentContentPage : BaseContentPage<ViewModels.DocumentContentViewModel>
+    {
+        FountainView.FountainContentView _fountainContentView;
+        public DocumentContentPage()
+        {
+            _fountainContentView = new FountainView.FountainContentView();
+            Content = _fountainContentView;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+
+        async void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "WebContent")
+            {
+                await _fountainContentView.SetContent(ViewModel.WebContent);
+            }
+        }
+    }
+}
