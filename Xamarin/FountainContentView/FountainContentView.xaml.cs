e782260ebecb4a1be55fd3abb9c21bba0af81513
Ben Buttigieg <benbtg@gmail.com>
2019-01-06 20:17:00 +0000

    Using custom Fountain view on all documents.

--- Xamarin/FountainContentView/FountainContentView.xaml.cs
+++ Xamarin/FountainContentView/FountainContentView.xaml.cs
@@ -5,30 +5,55 @@ using System.Threading.Tasks;
 using FountainSharp;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using FountainView;
+using System.Collections.ObjectModel;
 
 namespace FountainView
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FountainContentView : ContentView
     {
-        //public string FountainText { get; set; }
-        private FountainSharp.FountainDocument fountainDocument;
+        public void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
 
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(FountainContentView), default(string), propertyChanged: OnTextChanged);
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
 
-        List<FountainBlockElement> BlockElements { get; set; }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+        static async void OnTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (FountainContentView)bindable;
+
+            var existing = (string)oldValue;
+            var incoming = (string)newValue;
+
+            var doc = await FountainDocument.ParseAsync(incoming);
+
+            Xamarin.Essentials.MainThread.BeginInvokeOnMainThread(() =>
+            {
+                control.FountainList.ItemsSource = doc.Blocks.ToList();
+            });
+        }
+
+        private FountainDocument fountainDocument;
+        private ObservableCollection<FountainBlockElement> _blockElements = new ObservableCollection<FountainBlockElement>();
+        public ObservableCollection<FountainBlockElement> BlockElements { get => _blockElements; set => _blockElements = value; }
+
+        List<FountainBlockElement> Elements { get; set; }
         public FountainContentView()
         {
             InitializeComponent();
-        }
-
-        public async Task SetContent(string FountainText)
-        {
-            fountainDocument = await FountainSharp.FountainDocument.ParseAsync(FountainText);
-
-            FountainList.ItemsSource = fountainDocument.Blocks.ToList();
-
-            //var test = fountainDocument.Blocks.ToList()[2];
-           // Console.WriteLine(test);
         }
     }
 }
