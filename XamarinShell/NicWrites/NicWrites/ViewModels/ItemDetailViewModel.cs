5e5e36ab17698677040277c95cf1ec7ae153f7da
Ben Buttigieg <benbtg@gmail.com>
2019-01-09 15:08:57 +0000

    XamarinShell project

---
+++ XamarinShell/NicWrites/NicWrites/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NicWrites.Models;
+
+namespace NicWrites.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
